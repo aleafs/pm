22e111657471178c9c15af7a415411e921422931
aleafs <aleafs@aleafs-mac.local>
2012-01-26 17:12:11 +0800

    cluster.js开发完成

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
-var Master	= require('../lib/master.js').create();
+var Master	= require('../lib/cluster.js').create(true);
 
 Master.register(8080, __dirname + '/worker/api.js', 2);
 Master.dispatch();
