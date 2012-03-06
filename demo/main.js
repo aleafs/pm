53aa402127a36d26d39c84c899a03e66640bb19e
aleafs <zhangxc83@gmail.com>
2012-03-06 18:03:19 +0800

    Tab => two spaces

--- demo/main.js
+++ demo/main.js
@@ -20,27 +20,27 @@ app.register(
     /**
      * @起多少个进程, 默认为系统CPU数 (optional)
      */
-    'cnum'	: null,
+    'cnum'  : null,
 
     /**
      * @找不到可用worker时返回的消息体, 默认为HTTP协议的410消息
      */
-    'gone'	: '<!--ERROR-->',
+    'gone'  : '<!--ERROR-->',
 
     /**
      * @单个worker处理多少次请求后退出, 默认-1, 表示不启用这个功能
      */
-    'maxc'	: 1,
+    'maxc'  : 1,
 
-	/**
-	 * @worker进程的工作用户组，默认null，表示以master的启动用户组进行运行 (仅root下有效)
-	 */
-	'group'	: 'nobody',
+    /**
+     * @worker进程的工作用户组，默认null，表示以master的启动用户组进行运行 (仅root下有效)
+     */
+    'group' : 'nobody',
 
-	/**
-	 * @worker进程的工作用户，默认null，表示以master的启动用户进行运行 (仅root下有效)
-	 */
-	'user'	: 'nobody',
+    /**
+     * @worker进程的工作用户，默认null，表示以master的启动用户进行运行 (仅root下有效)
+     */
+    'user'  : 'nobody',
   }
 
 );
