aa23748a44c2eadcb0bd447c3d3081a391bae4c4
aleafs <zhangxc83@gmail.com>
2012-03-06 17:13:03 +0800

    增加多用户支持

--- demo/main.js
+++ demo/main.js
@@ -15,22 +15,35 @@ app.register(
    * @worker 的路径 (required)
    */
   __dirname + '/worker/echo.js', 
+  
+  {
+    /**
+     * @起多少个进程, 默认为系统CPU数 (optional)
+     */
+    'cnum'	: null,
 
-  /**
-   * @起多少个进程, 默认为系统CPU数 (optional)
-   */
-  null, 
+    /**
+     * @找不到可用worker时返回的消息体, 默认为HTTP协议的410消息
+     */
+    'gone'	: '<!--ERROR-->',
 
-  /**
-   * @找不到可用worker时返回的消息体, 默认为HTTP协议的410消息 (optional)
-   */
-  '<!--ERROR-->', 
+    /**
+     * @单个worker处理多少次请求后退出, 默认-1, 表示不启用这个功能
+     */
+    'maxc'	: 1,
 
-  /**
-   * @单个worker处理多少次请求后退出, 默认-1, 表示不启用这个功能 (optional)
-   */
-  1
+	/**
+	 * @worker进程的工作用户组，默认null，表示以master的启动用户组进行运行 (仅root下有效)
+	 */
+	'group'	: 'nobody',
+
+	/**
+	 * @worker进程的工作用户，默认null，表示以master的启动用户进行运行 (仅root下有效)
+	 */
+	'user'	: 'nobody',
+  }
+
 );
 
-app.register(33749, __dirname + '/worker/http.js', 1);
+app.register(33749, __dirname + '/worker/http.js', {'cnum' : 1});
 app.dispatch();
