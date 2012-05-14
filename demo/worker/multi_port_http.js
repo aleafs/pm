e673aaa036a667e50b9c0687a97107fdcc068e40
Jackson Tian <shyvo1987@gmail.com>
2012-05-15 00:37:33 +0800

    Update for coding style.

--- demo/worker/multi_port_http.js
+++ demo/worker/multi_port_http.js
@@ -11,19 +11,19 @@
 var http = require('http');
 var cluster = require('../../');
 
-var server33750 = http.createServer(function(req, res) {
+var server33750 = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
   res.end('hello world, handle port 33750');
 });
 
-var server33751 = http.createServer(function(req, res) {
+var server33751 = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
   res.end('hello world, handle port 33751');
 });
 
-cluster.ready(function(socket, port) {
+cluster.ready(function (socket, port) {
   console.log('%s:%s connect to %s', socket.remoteAddress, socket.remotePort, port);
-  if (port == 33750) {
+  if (port === 33750) {
     server33750.emit('connection', socket);
   } else {
     server33751.emit('connection', socket);
