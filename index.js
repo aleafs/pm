54abe9c02337e31b2516d01ff6a19c0e754adcea
aleafs <zhangxc83@gmail.com>
2012-05-27 00:11:31 +0800

    Add lib/worker.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
-exports.Master = function (options) {
+exports.Master  = function(options) {
   return require(__dirname + '/lib/master.js').create(options);
+};
+
+exports.Worker  = function() {
+  return require(__dirname + '/lib/worker.js').create();
 };
