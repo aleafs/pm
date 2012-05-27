64318496ccc70dae47c0370ff1e87c568ab70daf
aleafs <zhangxc83@gmail.com>
2012-05-27 12:06:14 +0800

    Add unittest for pidfile

--- test/cluster.test.js
+++ test/cluster.test.js
@@ -1,12 +1,27 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
+var fs      = require('fs');
 var should  = require('should');
 var Cluster = require(__dirname + '/../');
 
 describe('cluster interface', function () {
 
-  it('should_master_interface_works_fine', function () {
-    var _me = Cluster.Master();
+  /* {{{ should_master_create_pidfile_works_fine() */
+  it('should_master_create_pidfile_works_fine', function (done) {
+
+    var pid = __dirname + '/test.pid';
+    var _me = Cluster.Master({
+      'pidfile' : pid,
+    });
+    _me.dispatch();
+    setTimeout(function() {
+      fs.readFile(pid, 'utf-8', function(error, data) {
+        should.ok(!error);
+        parseInt(data, 10).should.eql(process.pid);
+        done();
+      });
+    }, 20);
   });
+  /* }}} */
 
 });
