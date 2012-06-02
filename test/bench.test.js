38d4ad70c1bbf4021dd3d9790ed825ff625b7401
aleafs <zhangxc83@gmail.com>
2012-06-02 19:43:57 +0800

    Add listen options split into an array

---
+++ test/bench.test.js
@@ -0,0 +1,10 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+var Master  = require(__dirname + '/../').Master({
+  'pidfile' : __dirname + '/bench.pid',
+});
+
+var server  = Master.register('http', __dirname + '/fixtures/http.js', {
+  'children'    : 1,
+    'listen'    : 7751,
+});
