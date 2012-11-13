ab88a7be0d137f11ff9618e826cc8313f3a1a332
aleafs <zhangxc83@gmail.com>
2012-11-13 17:48:52 +0800

    开发更新

---
+++ test/pair.test.js
@@ -0,0 +1,15 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+"use strict";
+
+var rewire = require('rewire');
+var should = require('should');
+var Common = require(__dirname + '/common.js');
+
+describe('worker pair', function () {
+
+  it('should_pair_init_works_fine', function () {
+  });
+
+});
+
